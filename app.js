07cb2cfa6c9339738a2cf219b654de1505f9dd40
shivam Shrivastava <shrivastavashivam560@gmail.com>
2023-08-04 00:48:48 +0530

    backend only

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ const payment = require("./routes/paymentRoute");
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
-app.use("/api/v1", payment);
+app.use("/api/v1", payment); 
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 // Middleware for Errors
 app.use(errorMiddleware);
